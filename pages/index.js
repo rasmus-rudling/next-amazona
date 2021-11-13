7adcd87f9f488ed7e8ff84749a7f7891eb0add2f
Rasmus Rudling <rasmusrudling@gmail.com>
2021-11-13 23:02:09 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Layout from '../components/Layout'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+	return (
+		<Layout>
+			<h1>Products</h1>
+
+			<ul>
+				<li>Product 1</li>
+				<li>Product 2</li>
+				<li>Product 3</li>
+			</ul>
+		</Layout>
+	)
+}
